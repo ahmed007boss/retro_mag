dfc6502514f2aa9288d7d3f5cf7a7e2c7377db23
DT-DARK-DANY <109741681+DT-DARK-DANY@users.noreply.github.com>
2024-05-28 23:18:14 +0300

    category_ID to CategoryId

--- app.py
+++ app.py
@@ -134,7 +134,7 @@ def get_all_magazine():
                 "NAME": merged_df2.loc[0]["NAME"],
                 "Headline": merged_df2.loc[0]["Headline"],
                 "Image": f"https://retromagapi.azurewebsites.net/images{image_df.loc[0]['image_url']}"
-                ,"CategoryID":merged_df2.loc[0]['category_ID']
+                ,"CategoryId":merged_df2.loc[0]['category_ID']
                 }
                 listofdata.append(magazine_data)
                 result["LatestFiveMagazines"]=listofdata
